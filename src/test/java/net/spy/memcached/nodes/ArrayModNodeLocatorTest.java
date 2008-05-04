6d88ee712e831a0d8adb5cc2e218a4b253a83206
Dustin Sallings <dustin@spy.net>
2008-05-04 16:45:08 -0700

    Swapped out NATIVE_HASH in favor of FNV1A_32_HASH for arraymod locator test.

--- src/test/java/net/spy/memcached/nodes/ArrayModNodeLocatorTest.java
+++ src/test/java/net/spy/memcached/nodes/ArrayModNodeLocatorTest.java
@@ -3,10 +3,6 @@ package net.spy.memcached.nodes;
 import java.util.Arrays;
 import java.util.Collection;
 
-import net.spy.memcached.nodes.ArrayModNodeLocator;
-import net.spy.memcached.nodes.HashAlgorithm;
-import net.spy.memcached.nodes.MemcachedNode;
-
 /**
  * Test the ArrayModNodeLocator.
  */
@@ -16,14 +12,14 @@ public class ArrayModNodeLocatorTest extends AbstractNodeLocationCase {
 	protected void setupNodes(int n) {
 		super.setupNodes(n);
 		locator=new ArrayModNodeLocator(Arrays.asList(nodes),
-			HashAlgorithm.NATIVE_HASH);
+			HashAlgorithm.FNV1A_32_HASH);
 	}
 
 	public void testPrimary() throws Exception {
 		setupNodes(4);
-		assertSame(nodes[3], locator.getPrimary("dustin"));
-		assertSame(nodes[0], locator.getPrimary("x"));
-		assertSame(nodes[1], locator.getPrimary("y"));
+		assertSame(nodes[2], locator.getPrimary("dustin"));
+		assertSame(nodes[3], locator.getPrimary("x"));
+		assertSame(nodes[0], locator.getPrimary("y"));
 	}
 
 	public void testAll() throws Exception {
@@ -38,12 +34,12 @@ public class ArrayModNodeLocatorTest extends AbstractNodeLocationCase {
 
 	public void testSeq1() {
 		setupNodes(4);
-		assertSequence("dustin", 0, 1, 2);
+		assertSequence("dustin", 3, 0, 1);
 	}
 
 	public void testSeq2() {
 		setupNodes(4);
-		assertSequence("noelani", 1, 2, 3);
+		assertSequence("noelani", 0, 1, 2);
 	}
 
 	public void testSeqOnlyOneServer() {
@@ -53,6 +49,6 @@ public class ArrayModNodeLocatorTest extends AbstractNodeLocationCase {
 
 	public void testSeqWithTwoNodes() {
 		setupNodes(2);
-		assertSequence("dustin", 0);
+		assertSequence("dustin", 1);
 	}
 }
